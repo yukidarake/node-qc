bbd43fbd20e2a4f8a72429dff650d3aa9f91cbc7
Toshiyuki Nakamura <yukidarake4@gmail.com>
2013-10-04 20:16:04 +0900

    initial commit

---
+++ test/index.js
@@ -0,0 +1,29 @@
+var qc = require('../lib');
+
+describe('lib/index.js', function() {
+    describe('wrap', function() {
+        it('マルチバイトだけ', function() {
+            qc.wrap('ほげ').should.equal(
+                '＿人人人人＿\n' +
+                '＞  ほげ  ＜\n' +
+                '￣Y^Y^Y^Y^￣'
+            );
+        });
+
+        it('マルチバイトなし', function() {
+            qc.wrap('ab12').should.equal(
+                '＿人人人人＿\n' +
+                '＞  ab12  ＜\n' +
+                '￣Y^Y^Y^Y^￣'
+            );
+        });
+
+        it('マルチバイトと混在', function() {
+            qc.wrap('ほげ1混在').should.equal(
+                '＿人人人人人人人＿\n' +
+                '＞  ほげ1混在   ＜\n' +
+                '￣Y^Y^Y^Y^Y^Y^Y^￣'
+            );
+        });
+    });
+});
