c6ab6b55c60ce37e0abaad0841c7943f02ffd73b
Toshiyuki Nakamura <yukidarake4@gmail.com>
2013-10-04 20:55:30 +0900

    Fixed ずれずれ問題

--- test/index.js
+++ test/index.js
@@ -5,24 +5,24 @@ describe('lib/index.js', function() {
         it('マルチバイトだけ', function() {
             qc.wrap('ほげ').should.equal(
                 '＿人人人人＿\n' +
-                '＞  ほげ  ＜\n' +
-                '￣Y^Y^Y^Y^￣'
+                '＞　ほげ　＜\n' +
+                '￣Y^Y^Y^￣'
             );
         });
 
         it('マルチバイトなし', function() {
             qc.wrap('ab12').should.equal(
                 '＿人人人人＿\n' +
-                '＞  ab12  ＜\n' +
-                '￣Y^Y^Y^Y^￣'
+                '＞　ab12　＜\n' +
+                '￣Y^Y^Y^￣'
             );
         });
 
         it('マルチバイトと混在', function() {
             qc.wrap('ほげ1混在').should.equal(
                 '＿人人人人人人人＿\n' +
-                '＞  ほげ1混在   ＜\n' +
-                '￣Y^Y^Y^Y^Y^Y^Y^￣'
+                '＞　ほげ1混在　＜\n' +
+                '￣Y^Y^Y^Y^Y^Y^￣'
             );
         });
     });
